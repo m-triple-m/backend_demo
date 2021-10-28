ddec8b41e88c0b4d189fb752b03ffe2c6337706c
m-triple-m <triplem656@gmail.com>
2021-10-28 16:36:08 +0530

    ..

--- index.js
+++ index.js
@@ -9,6 +9,11 @@ app.use(express.json());
 app.use(cors({ origin: ["http://localhost:3000"] }));
 app.use("/user", userRouter);
 
+app.get("/", (req, res) => {
+  console.log("request accepted");
+  res.send("Server Online!!");
+});
+
 app.listen(port, () => {
   console.log("server started ...");
 });
